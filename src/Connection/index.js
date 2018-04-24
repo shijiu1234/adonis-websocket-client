bc51bb5116d5288b75545560e4988f4b64bf3208
Joe <sjshijiu@qq.com>
2018-04-24 11:24:12 +0800

    Update index.js
    avoid error on react native environment

--- src/Connection/index.js
+++ src/Connection/index.js
@@ -22,7 +22,7 @@ import JsonEncoder from '../JsonEncoder/index.js'
  * @returns {String}
  *
  */
-const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
+const wsProtocol = (window && window.location && window.location.protocol === 'https:') ? 'wss' : 'ws'
 
 /**
  * Connection class is used to make a TCP/Socket connection
